84fc8361ca9472ee129234046b700b4bf677b176
lucmachuca <luc.machuca@duocuc.cl>
2025-11-13 23:04:50 -0300

    init commit

---
+++ src/main/java/com/levelupgamer/levelupbackend/LevelupbackendApplication.java
@@ -0,0 +1,13 @@
+package com.levelupgamer.levelupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelupbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelupbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levelupgamer/levelupbackend/LevelupbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelupgamer.levelupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
